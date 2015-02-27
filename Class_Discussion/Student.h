3809494b66590cf18b717807db1544e9c3434b1f
Oliver Hill <oliverhill73@aol.com>
2015-02-27 10:51:01 -0500

    Student properties

---
+++ Class_Discussion/Student.h
@@ -0,0 +1,15 @@
+//
+//  Student.h
+//  Class_Discussion
+//
+//  Created by Hill, Oliver on 2/27/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+@property NSInteger *grade;
+@property NSInteger *year;
+@property NSString *fullName;
+
+@end
