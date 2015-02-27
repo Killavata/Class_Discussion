31e22c4acbce4199969f7b19ad53bcd32242e50b
Killavata <severovstepan@gmail.com>
2015-02-27 10:23:08 -0500

    Some minor changes

---
+++ Class_Discussion/Account.h
@@ -0,0 +1,15 @@
+//
+//  Account.h
+//  Class_Discussion
+//
+//  Created by sseverov on 2/25/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Account : NSObject
+
+@property NSString* type;
+
+@end
---
+++ Class_Discussion/Account.m
@@ -0,0 +1,13 @@
+//
+//  Account.m
+//  Class_Discussion
+//
+//  Created by sseverov on 2/25/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import "Account.h"
+
+@implementation Account
+
+@end
