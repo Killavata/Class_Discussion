7837fc50d661abf9daabe708c5f6e8eb476ec99f
Oliver Hill <oliverhill73@aol.com>
2015-02-27 10:24:07 -0500

    Basic Student Properties

---
+++ Class_Discussion/Student.h
@@ -0,0 +1,19 @@
+//
+//  Student.h
+//  Class_Discussion
+//
+//  Created by Hill, Oliver on 2/25/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Student : NSObject
+@property (nonatomic) NSInteger year;
+@property (nonatomic) NSInteger grade;
+@property (nonatomic) UIImage *studentPicture;
+
+
+
+@end
---
+++ Class_Discussion/Student.m
@@ -0,0 +1,14 @@
+//
+//  Student.m
+//  Class_Discussion
+//
+//  Created by Hill, Oliver on 2/25/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+//This is the student class
+#import "Student.h"
+
+@implementation Student
+
+
+@end
