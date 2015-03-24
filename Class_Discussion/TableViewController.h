726b37a7e34b71c6226a1eb0da63ae8fca593134
lbakker <lbakker@deerfield.edu>
2015-03-24 10:20:04 -0400

    alkdjf

---
+++ Class_Discussion/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Class_Discussion
+//
+//  Created by Luke Bakker on 3/23/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UIViewController
+
+@end
---
+++ Class_Discussion/TableViewController.m
@@ -0,0 +1,28 @@
+//
+//  TableViewController.m
+//  Class_Discussion
+//
+//  Created by Luke Bakker on 3/23/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
