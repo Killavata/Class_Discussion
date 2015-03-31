4139d2d4adf109d706dee2a3b58c15df0c7f7c4e
Oliver Hill <oliverhill73@aol.com>
2015-03-31 09:54:28 -0400

    updated navigation controller
    added view controller files to go with each new view controller, put
    them in the login screen folder

---
+++ Class_Discussion/ForgotPasswordViewController.m
@@ -0,0 +1,37 @@
+ //
+//  ForgotPasswordViewController.m
+//  Class_Discussion
+//
+//  Created by Hill, Oliver on 3/25/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import "ForgotPasswordViewController.h"
+
+@interface ForgotPasswordViewController ()
+
+@end
+
+@implementation ForgotPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
