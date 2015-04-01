d6e70e941fe810cd4efd77b33705bd9e44287136
Killavata <severovstepan@gmail.com>
2015-04-01 11:57:19 -0400

    Revert aa959e9..1a9df45
    This rolls back to commit aa959e9e3eea41d791f5b050cf719156d3b21919.

---
+++ Class_Discussion/CreateAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateAccountViewController.h
+//  Class_Discussion
+//
+//  Created by Hill, Oliver on 3/25/15.
+//  Copyright (c) 2015 Stepan Severov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateAccountViewController : UIViewController
+
+@end
--- Class_Discussion/TableViewController.h
+++ Class_Discussion/TableViewController.h
@@ -8,7 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TableViewController : UITableViewController
-
+@interface TableViewController : UIViewController
 
 @end
